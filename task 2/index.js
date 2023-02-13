cd987daf032b79c5ef617f11ec84a7efea2200e9
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2023-02-13 10:22:43 +0200

    Update index.js

--- task 2/index.js
+++ task 2/index.js
@@ -1,4 +1,14 @@
 let p = document.createElement("p");
-
 document.body.appendChild(p);
-p.textContent = "This is test!"
+p.textContent = "This is test!";
+
+let pets = document.createElement("select");
+document.body.appendChild(pets);
+pets.name = "pets";
+
+let cat = document.createElement("option");
+pets.appendChild(cat);
+
+cat.value = "cat";
+cat.innerText = "Cat";
+
