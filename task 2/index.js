b5d3166173d3a8d6d6ee365bf764486dc58539e6
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2023-02-13 10:53:26 +0200

    Update index.js

--- task 2/index.js
+++ task 2/index.js
@@ -1,10 +1,11 @@
-let p = document.createElement("p");
+let p = document.createElement("h1");
 document.body.appendChild(p);
 p.textContent = "Which is your favorite pet?";
 
 let pets = document.createElement("select");
 document.body.appendChild(pets);
 pets.name = "pets";
+pets.className = "pets";
 
 let chose = document.createElement("option");
 pets.appendChild(chose);
@@ -14,14 +15,29 @@ chose.innerText = "Chose pet";
 let cat = document.createElement("option");
 pets.appendChild(cat);
 cat.value = "cat";
-cat.innerText = "Cat";
+cat.innerText = "cat";
 
 let dog = document.createElement("option");
 pets.appendChild(dog);
 dog.value = "dog";
-dog.innerText = "Dog";
+dog.innerText = "dog";
 
 let horse = document.createElement("option");
 pets.appendChild(horse);
 horse.value = "horse";
-horse.innerText = "Horse";
+horse.innerText = "horse";
+
+let text = document.createElement("h2");
+document.body.appendChild(text);
+text.innerText = "PET";
+
+let img = document.createElement("img");
+document.body.appendChild(img);
+img.style.height = "25%";
+img.style.width = "60%"
+
+
+pets.addEventListener("change", (e) => {
+    text.textContent = `${e.target.value}`;
+    img.src = `./images/${e.target.value}.jpg`
+})
