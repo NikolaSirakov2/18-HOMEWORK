9c99beb0e7a833f71e9f72dca354696269173ba0
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2023-02-17 18:35:44 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,27 +1,15 @@
-class Animal {
-    constructor (name, age) {
-        this.name = name;
-        this.age = age;
-    }
+let myObj = {
+    name: "koko",
+    age: 30
+};
+
+function Myfunc(ob){
+        ob = null;
+
+        return ob.name;
 }
 
-Animal.prototype.bite = function () {
-    return `This animal can bite!`;
-}
 
-class Dog extends Animal {
+console.log(Myfunc(myObj));
 
-    constructor (name, color){
-        super(name);
-        this.color = color;
-    }
-}
-
-Dog.prototype.run = function () {
-    return `Can run!`
-}
-
-let rex = new Dog ("Rex", "brown");
-
-console.log(rex.__proto__);
-console.log(rex.__proto__.__proto__);
+console.log(myObj);
