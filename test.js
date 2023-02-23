d2db1078b0746c004459e0d27791b0612b20d696
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2023-02-23 13:52:03 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,22 +1,27 @@
 const users = [
-    {
-        id: 1,
-        name: "Nik",
-        isActive: true
-    },
-    {
-        id: 1,
-        name: "Papa",
-        isActive: true
-    },
-    {
-        id: 1,
-        name: "Aron",
-        isActive: true
-    }
-]
+  {
+    id: 5,
+    name: "Nik",
+    isActive: true,
+  },
+  {
+    id: 2,
+    name: "Papa",
+    isActive: false,
+  },
+  {
+    id: 3,
+    name: "Aron",
+    isActive: true,
+  },
+];
 
-const namesArr = users.map(e => e.name)
+const namesArr = users
+  .sort((a, b) => {
+    b.id - a.id;
+  })
+  .filter((e) => e.isActive)
+  .map((user) => user.name);
 
 console.log(users);
 console.log(namesArr);
