8ce52a025c187a1beeb67d7edd27b15771d81dcd
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2023-02-25 11:26:58 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,19 +1,13 @@
-function capitalize(string) {
-  let arr = string.split("<div>");
-
-  let newArr = [];
-
-  for (let i = 0; i < arr.length; i++) {
-    if (i % 2 !== 0) {
-      newArr.push("<div>");
-      newArr.push(arr[i]);
-      newArr.push("</div>");
-    } else {
-      newArr.push(arr[i]);
-    }
+const person = {
+  name: "Nik",
+  sayName: function(){
+    return this.name;
   }
-
-  return newArr.join("");
 }
 
-console.log(capitalize("<div><div><p>Hello</p><div><div>"));
+console.log(person.sayName());
+let name = "Bil";
+
+let globalFunc = person.sayName;
+
+globalFunc();
