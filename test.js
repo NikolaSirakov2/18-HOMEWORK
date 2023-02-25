0069852fc2fa8b6694ace1632e617c58fb9f8555
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2023-02-25 08:54:38 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,8 +1,15 @@
-person = {
-  name: "Nik",
-    sayName: (name) => {
-        console.log(this.name);
-  }
+function capitalize(string){
+  let newString = "";
+
+  for(let i = 0; i < newString.length; i+=2){
+    if(i % 2 === 0){
+      newString += string[i].toUpperCase();
+    } else {
+      newString += string[i].toLowerCase();
+    }
+  };
+
+  return newString;
 }
 
-person.sayName("nik");
+console.log(capitalize("Hello"));
