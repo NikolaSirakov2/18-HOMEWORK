dcbfe12c48a1f42a157b702155086bbcb35daf3c
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2023-02-27 07:57:15 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,13 +1,27 @@
-class Person {
-  constructor(name) {
-    this.name = name;
-  }
+// class Person {
+//   constructor(name) {
+//     this.name = name;
+//   }
 
-  run = () => {
-    console.log(`${this.name} is runing!`);
-  }
+//   run = () => {
+//     console.log(`${this.name} is runing!`);
+//   }
+// }
+
+// let human = new Person("Mad");
+
+// human.run();
+
+let old = {
+  value: 9
 }
 
-let human = new Person("Mad");
+const change = (num) => {
+  num.value = 3;
+  return num;
+}
 
-human.run();
+let newN = change(old);
+
+console.log(old);
+console.log(newN);
