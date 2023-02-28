2ac5415a0f793a807a177fdb83dc6e8f2b32662e
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2023-02-28 10:17:43 +0200

    Update index.js

--- task 3/index.js
+++ task 3/index.js
@@ -7,14 +7,14 @@ image.src = "./img.jpg";
 image.style.display = "flex"
 image.style.position = "absolute";
 image.style.width = "600px";
-image.style.top = "250px";
-image.style.left = "700px";
+// image.style.top = "250px";
+// image.style.left = "700px";
 
 
 
 // let bottom = 0;
-let y = 250;
-let x = 700;
+let y = 0;
+let x = 0;
 // let right = 0;
 
 document.addEventListener("keydown", pressArrowKey);
@@ -23,16 +23,16 @@ function pressArrowKey(e) {
   if (e.keyCode === 38 && x > 0) {
     x -= 10;
     image.style.top = `${x}px`;
-  } else if (e.keyCode === 40 && x < 700) {
+  } else if (e.keyCode === 40 && x < 590) {
     x += 10;
     image.style.top = `${x}px`
   } else if (e.keyCode === 37 && y > 0) {
     y -= 10;
     console.log(y);
     image.style.left = `${y}px`;
-  } else if (e.keyCode === 39 && y < 1280) {
+  } else if (e.keyCode === 39 && y < 1320) {
     y += 10;
-    console.log(y);
+    
     image.style.left = `${y}px`;
   }
 }
