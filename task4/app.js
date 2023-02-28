ca7248110a5979fa534f0feb5881aaef8e2dd8de
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2023-02-28 10:56:29 +0200

    Update app.js

--- task4/app.js
+++ task4/app.js
@@ -1,3 +1,22 @@
 let form = document.createElement("form");
 document.body.appendChild(form);
+form.style.display = "inline-block";
+form.style.background = "pink";
+form.style.gap = "10px";
 
+let redInput = document.createElement("input");
+form.appendChild(redInput);
+redInput.id = "red";
+
+let greenInput = document.createElement("input");
+form.appendChild(greenInput);
+greenInput.id = "green";
+
+let blueInput = document.createElement("input");
+form.appendChild(blueInput);
+blueInput.id = "blue";
+
+let button = document.createElement("button");
+form.appendChild(button);
+button.style.width = "200px";
+button.style.height = "50px";
