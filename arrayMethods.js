290bb9d3b28cc15f69f859b78b3b319d58b1ca08
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2023-03-01 16:50:43 +0200

    Create arrayMethods.js

---
+++ arrayMethods.js
@@ -0,0 +1,9 @@
+let arr = [
+    {name: "Nik", age: 30, isActive: true},
+    
+    {name: "Kory", age: 20, isActive: false},
+    
+    {name: "Aron", age: 30, isActive: false},
+    
+    {name: "Za", age: 30, isActive: true},
+]
