44c4011ab94e31278c9a703226640f4d5d584973
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2023-03-01 17:14:57 +0200

    Update arrayMethods.js

--- arrayMethods.js
+++ arrayMethods.js
@@ -9,9 +9,13 @@ let arr = [
 ];
 
 // let test = arr.filter(e => e.isActive === true && e.age > 37 && e.name.length > 4);
-
-let test = arr.slice().pop();
+// let test = arr.slice(2,4);
+// let newA = test.concat(arr);
+// let test = arr.map(e => e.name)
+// let test = `Average age = ${arr.reduce((start, e) => {
+//     return start + e.age;
+// }, 0)/arr.length}`;
 
 console.log(arr);
-
 console.log(test);
+
